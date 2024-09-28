8da9519725299b60a3d816c1aa22a843f0975a8f
Catherine <z222ye@uwaterloo.ca>
2024-09-28 14:47:13 -0400

    imple main module, j_k_logic

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Evan & Catherine
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -25,3 +25,36 @@ module tt_um_example (
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+
+module main (
+  inout wire[3:0] bits_in_out,
+  input wire clk,
+  input wire clr_n,
+  input wire lp,
+  input wire cp,
+  input wire ep
+)
+  output reg[3:0] counter;
+  set_counter_bit(clr_n, lp, cp, bits_in_out[0], 1, clk, counter[0]);
+  set_counter_bit(clr_n, lp, cp, bits_in_out[1], (counter[0]), clk, counter[1]);
+  set_counter_bit(clr_n, lp, cp, bits_in_out[2], (counter[0] & counter[1]), clk, counter[2]);
+  set_counter_bit(clr_n, lp, cp, bits_in_out[3], (counter[0] & counter[1] & counter[2]), clk, counter[3]);
+  assign bits_in_out = ep? counter : 4'bZZZZ;
+endmodule
+
+module j_k_logic (
+  input pclr,
+  input lp,
+  input cp,
+  input bn,
+  input a,
+  output j,
+  output k
+);
+  wire plp_cp_a;
+  assign plp_cp_a = ~lp & cp & a;
+
+  assign j = (pclr & plp_cp_a) | (pclr & lp & bn);
+  assign k = (~pclr) | (plp_cp_a) | (lp & ~bn);
+endmodule
+

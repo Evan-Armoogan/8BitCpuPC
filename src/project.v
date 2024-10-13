53675087976a8991e88ded3ef41d70a080dd0419
Evan Armoogan <evanjarmoogan@gmail.com>
2024-10-12 21:25:08 -0400

    Update top level module

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module program_counter_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,13 +16,17 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire[3:0] data;
+  assign data[3:0] = uio_in[3:0]
+  ProgramCounter pc(
+    data,
+    clk,
+    ui_in[0],
+    ui_in[1],
+    ui_in[2],
+    ui_in[3]
+  );
+  assign uio_out[3:0] = data[3:0]
 
 endmodule
 

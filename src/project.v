a37b25d18bfde543462e1ac30da50a18ecb0c876
Evan Armoogan <evanjarmoogan@gmail.com>
2024-10-12 21:57:23 -0400

    Missing comma

--- src/project.v
+++ src/project.v
@@ -33,7 +33,7 @@ module tt_um_program_counter_top_level (
   assign uio_out[7:4] = 0;
   assign uio_oe  = 0;
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, ui_in[7], ui_in[6], ui_in[5], ui_in[4], ui_in[3] 1'b0};
+  wire _unused = &{ena, ui_in[7], ui_in[6], ui_in[5], ui_in[4], ui_in[3], 1'b0};
 
 endmodule
 

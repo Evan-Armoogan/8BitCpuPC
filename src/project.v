bb0c257006ed6d97aecc08738436767ce3a823d3
Evan Armoogan <evanjarmoogan@gmail.com>
2024-10-12 21:41:56 -0400

    Rename top level module

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module program_counter_top (
+module tt_um_program_counter_top_level (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
